120cab8a07e29262e93d9c1f927a3309effbd249
Soumyasri2000 <soumya.sri4920@gmail.com>
2025-06-24 12:15:58 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,31 @@
+import time
+import os
+import logging
+import logfunc
+import RPi.GPIO as GPIO
+import board
+
+from TempHum import _read_channel,getOutdoor,getIndoor,getRoomTemp,Temperature,calculate_Wet_bulb,saturation_vapour_pressure
+from StartupSequnece import Startup_sequence
+from MQTTcomm import MqttConnection, mqttClient
+from Systemcontrol import SelfCareRoutine,setup,LoopModule
+from GSMmodule import power_on_modem,send_At,connect_gpre,gsm_config,init_gsm,on_connect,on_message
+from Firmware_ota import newCommandReceived, upload_message,parse_json_data ,Set_Point_Temperature ,is_emergency_message
+from Motor import start_motor, stop_motor, setspeed
+from Pump import checkPumpFlow, checkWaterLevel
+
+def main():
+
+   #call setup function
+    setup()
+    logging.info("Setup complete, entering main loop")
+    try:
+        while True:
+            LoopModule()
+    except KeyboardInterrupt:
+        logging.info(f"Stopped by user")
+    finally:
+         # Cleanup GPIO and any other services
+        GPIO.cleanup()
+        logging.info("GPIO cleaned up, shutdown complete")
 
